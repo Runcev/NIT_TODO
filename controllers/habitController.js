431a85f12637ae44b2dd2aa51efd5d2092717a52
AnastasiaSN <65014597+AnastasiaSN@users.noreply.github.com>
2020-11-19 21:47:33 +0200

    модель Habit - додавання/редагування даних

--- controllers/habitController.js
+++ controllers/habitController.js
@@ -5,11 +5,8 @@ exports.habitList = function (req, res) {
     Habit.find({})
         .exec(function (err, habits) {
             if (err) {
-                return next(err);
+                console.log(err);
             }
-
-            //console.log(JSON.stringify(users));
-
 
             if (req.baseUrl.match(/api/)) {
                 res.send({ habits: JSON.stringify(habits) });
@@ -28,22 +25,64 @@ exports.habitDetail = function (req, res) {
     }
 };
 
+// виводимо форму для створення нового запису
+exports.habitAddGet = function (req, res) {
+    res.render('habit/habitAdd', {title: 'Add habit'});
+}
+
+// додаємо
 exports.habitAdd = function (req, res) {
+
+    let name = req.body.name;
+
+    // юзер у нас завжди доступний після авторизації
+    let user = req.currentUser;
+
+    var habit = new Habit({name: name, user: user, isActual: true});
+
+    habit.save(function (err) {
+
+        if (err) console.log(err);
+    });
 
     if (req.baseUrl.match(/api/)) {
         res.send('');
     } else {
-        res.render('index', {title: 'Scheduler'});
+        res.redirect('/habit');
     }
+};
+
+// виводимо форму для редагування
+exports.habitEdit = function (req, res) {
+
+    let habitId = req.params.id;
+
+    Habit.findById(habitId).exec(function (err, habit) {
+        if (err) {
+            if (err) console.log(err);
+        }
+
+        res.render('habit/habitEdit', {title: 'Edit habit', habit: habit});
+    });
 };
 
 exports.habitUpdate = function (req, res) {
 
-    if (req.baseUrl.match(/api/)) {
-        res.send('');
-    } else {
-        res.render('index', {title: 'Scheduler'});
-    }
+    let name = req.body.name;
+
+    let isActual = req.body.isActual??false;
+
+    var habit = new Habit({name: name, isActual: isActual, _id: req.params.id});
+
+    Habit.findByIdAndUpdate(req.params.id, habit, {}, function (err, habit) {
+        if (err) console.log(err);
+
+        if (req.baseUrl.match(/api/)) {
+            res.send('');
+        } else {
+            res.redirect('/habit');
+        }
+    });
 };
 
 exports.habitDelete = function (req, res) {
