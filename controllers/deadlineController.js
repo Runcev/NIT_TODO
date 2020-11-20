7ff87c7cd73bdbcb1ed396867494d5b35b4a805c
Vitya <vitalii.kenyiz@ukma.edu.ua>
2020-11-20 17:11:47 +0200

    adde almost all controllers

--- controllers/deadlineController.js
+++ controllers/deadlineController.js
@@ -1,11 +1,20 @@
 
+var Deadline = require('../models/deadlineModel')
+
 exports.deadlineList = function (req, res) {
 
-    if (req.baseUrl.match(/api/)) {
-        res.send('');
-    } else {
-        res.render('index', {title: 'Scheduler'});
-    }
+    Deadline.find({})
+        .exec(function(err,deadlines){
+            if(err){
+                console.log(err);
+            }
+
+            if (req.baseUrl.match(/api/)) {
+                res.send({ deadlines : JSON.stringify(deadlines) });
+            } else {
+                res.render('deadline/deadlineList', {title: 'Deadline list', deadlines: deadlines});
+            }
+        });
 };
 
 exports.deadlineDetail = function (req, res) {
@@ -17,22 +26,48 @@ exports.deadlineDetail = function (req, res) {
     }
 };
 
+// виводимо форму для створення нового запису
+exports.deadlineAddGet = function (req, res) {
+    res.render('deadline/deadlineAdd', {title: 'Add deadline'});
+}
+
+// додаємо
 exports.deadlineAdd = function (req, res) {
+
+    let name = req.body.name;
+    let entity = req.entity;
+    let date = req.date;
+
+    var deadline = new Deadline({date: date,name: name, entity: entity});
+
+    deadline.save(function(err){
+        if(err) console.log(err);
+    })
 
     if (req.baseUrl.match(/api/)) {
         res.send('');
     } else {
-        res.render('index', {title: 'Scheduler'});
+        res.redirect('/deadline'); 
     }
 };
 
 exports.deadlineUpdate = function (req, res) {
 
-    if (req.baseUrl.match(/api/)) {
-        res.send('');
-    } else {
-        res.render('index', {title: 'Scheduler'});
-    }
+    let name = req.body.name;
+    let entity = req.entity;
+    let date = req.date;
+
+    var deadline = new Deadline({date: date,name: name, entity: entity});
+
+    Deadline.findByIdAndUpdate(req.params.id, deadline, {}, function(err, deadline){
+        if(err) console.log(err);
+
+        if (req.baseUrl.match(/api/)) {
+            res.send('');
+        } else {
+            res.redirect('/deadline'); 
+        }
+    });
 };
 
 exports.deadlineDelete = function (req, res) {
--- controllers/topicController.js
+++ controllers/topicController.js
@@ -1,11 +1,19 @@
+var Topic = require('../models/topicModel')
 
 exports.topicList = function (req, res) {
 
-    if (req.baseUrl.match(/api/)) {
-        res.send('');
-    } else {
-        res.render('index', {title: 'Scheduler'});
-    }
+    Topic.find({})
+    .exec(function (err, topics) {
+        if (err) {
+            console.log(err);
+        }
+
+        if (req.baseUrl.match(/api/)) {
+            res.send({ topics: JSON.stringify(topics) });
+        } else {
+            res.render('topic/topicList', {title: 'Topics list', topics: topics});
+        }
+    });
 };
 
 exports.topicEventsList = function (req, res) {
@@ -26,22 +34,49 @@ exports.topicDetail = function (req, res) {
     }
 };
 
+// виводимо форму для створення нового запису
+exports.topicAddGet = function (req, res) {
+    res.render('topic/topicAdd', {title: 'Add topic'});
+}
+
 exports.topicAdd = function (req, res) {
+
+    let name = req.body.name;
+
+    // юзер у нас завжди доступний після авторизації
+    let user = req.currentUser;
+
+    var topic = new Topic({name: name, user: user});
+
+    topic.save(function(err){
+        if(err) console.log.err;
+    });
 
     if (req.baseUrl.match(/api/)) {
         res.send('');
     } else {
-        res.render('index', {title: 'Scheduler'});
+        res.redirect('/topic');
     }
 };
 
 exports.topicUpdate = function (req, res) {
+    
+    let name = req.body.name;
 
-    if (req.baseUrl.match(/api/)) {
-        res.send('');
-    } else {
-        res.render('index', {title: 'Scheduler'});
-    }
+    // юзер у нас завжди доступний після авторизації
+    let user = req.currentUser;
+
+    var topic = new Topic({name: name, user: user, _id: req.params.id});
+
+    Topic.findByIdAndUpdate(req.params.id, topic, {}, function(err,topic){
+        if(err) console.log(err);
+
+        if (req.baseUrl.match(/api/)) {
+            res.send('');
+        } else {
+            res.redirect('/topic');
+        }
+    });
 };
 
 exports.topicDelete = function (req, res) {
