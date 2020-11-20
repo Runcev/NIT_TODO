cb44c9a2bc6adcd258b85ef2f916ba1204e05c57
Vitya <vitalii.kenyiz@ukma.edu.ua>
2020-11-20 02:40:12 +0200

    change ui of page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,43 @@
 import React, {useState, useEffect} from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from "moment"
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+
 import './App.css';
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
 import UserList from "./components/UserList";
 
-function App() {
+
+const localizer = momentLocalizer(moment)
+const DnDCalendar = withDragAndDrop(Calendar);
+
+function App(){
+   
+    const events = [
+        {
+            start: moment().toDate(),
+            end: moment().add("days").toDate(),
+            title: "Some title", 
+        }
+    ];
+
+    const onEventResize = (data) => {
+        const { start, end } = data;
+    
+        this.setState(() => {
+          events[0].start = start;
+          events[0].end = end;
+          return { events: [...events] };
+        });
+      };
+    
+    const onEventDrop = (data) => {
+        console.log(data);
+      };
+      
+
 
     const callApi = async () => {
         const res = await fetch('/api/user');
@@ -77,32 +112,51 @@ function App() {
 
     return (
         <div className="App">
+            <div className ="head">
+                <h1>Scheduler - Test ReactApp</h1>
+                <p>{responseToPost}</p>
+            </div>
 
-            <h1>Scheduler - Test ReactApp</h1>
-
-            {users}
-
-            <form onSubmit={handleSubmit}>
-                <p>
-                    <strong>Add user:</strong>
-                </p>
-                <div>Name: <input
+            <div className ="users">
+                {users}
+            </div>
+            <div className = "block-bottom">
+            <form className="box" onSubmit={handleSubmit}>
+                 <h1>Login</h1>
+               <input
                     type="text"
+                    placeholder="Username"
                     value={name}
                     onChange={e => setName(e.target.value)}
-                /></div>
+                />
 
-                <div>Email: <input
+                <input
                     type="text"
+                    placeholder="Email"
                     value={email}
                     onChange={e => setEmail(e.target.value)}
-                /></div>
+                />
 
-                <button type="submit">Submit</button>
+                <input type ="submit" value="Login"/>
             </form>
-            <p>{responseToPost}</p>
-        </div>
-    );
+            </div>
+
+            {/* <div className="calendar">
+                <DnDCalendar
+                    defaultDate={moment().toDate()}
+                    defaultView="month"
+                    events={events}
+                    localizer={localizer}
+                    onEventDrop={onEventDrop}
+                    onEventResize={onEventResize}
+                    resizable
+                    style={{ height: "100vh" }}
+                />
+
+            </div> */}
+            
+        </div>        
+    ); 
 }
 
 export default App;
--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -2,6 +2,13 @@ import React from "react";
 
 export default function UserList(user) {
     return (
-        <div>{user.name} {user.email}</div>
+        <div>
+            <ul>
+                <li>
+                    Name: {user.name}<br></br>
+                    Email: {user.email}
+                </li>
+            </ul>
+        </div>
     );
 }
